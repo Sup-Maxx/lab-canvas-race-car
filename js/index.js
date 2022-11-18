daf59fa88ca3d6fa8cb15c170c1b53b6964d5ba1
Maksym Kopychanskyi <kopy.chan.max@gmail.com>
2022-11-18 22:14:15 +0000

    made it through first iteration. the lab is way more advanced than the theory from the given lesson.

--- js/index.js
+++ js/index.js
@@ -1,7 +1,36 @@
+//the basis of the canvas
+const image = document.getElementById("road")
+
+const canvas = document.querySelector("canvas")
+const ctx = canvas.getContext("2d")
+
+//Road Map to the Canvas
+function copyImageToCanvas() {
+  ctx.drawImage(image, 0, 0, 500, 650) 
+  
+}
+
+setTimeout(() => {copyImageToCanvas()},);
+
+//get the car to the canvas 
+const carImg = new Image()
+
+carImg.src ="../images/car.png"
+let carImgX = 0
+let carImgY = 0
+
+function draw(x,y) {
+  ctx.clearRect(0, 0, 700, 700);
+  ctx.drawImage(carImg, carImgX, carImgY, 50,50)
+}
+
+
 window.onload = () => {
   document.getElementById('start-button').onclick = () => {
     startGame();
   };
+  
 
   function startGame() {}
 };
+
